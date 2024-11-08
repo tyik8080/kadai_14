dcf1ac66e1dc0edd69d25d3f8f942295aae3de2c
tyik8080 <tyik8080@yahoo.co.jp>
2024-11-08 22:06:54 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,8 @@
+ 
+ // 関数 calculateTotal を定義し、引数 num を受け取ります
+ const calculateTotal = (num) => {
+ // 引数 num の 2 乗を計算し、その結果をコンソールに出力します
+    console.log(num**2);
+  }
+  // 関数 calculateTotal を呼び出し、引数として 10 を渡します
+  calculateTotal(10);  // コンソールには 10 の 2 乗である 100 が出力されます
